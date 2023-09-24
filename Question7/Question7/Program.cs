9fae266f82b6e70ed8a8b64a158398d0995303f8
Shiv Sharma <105107525+shivsharma23@users.noreply.github.com>
2023-09-24 17:54:47 +0530

    Add files via upload

---
+++ Question7/Question7/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("ENTER STRING:");
+string x = Console.ReadLine();
+char[] arr = new char[x.Length];
+for (int i = 0; i < x.Length; i++)
+{
+    arr[i] = x[i];
+}
+int n=arr.Length;
+for (int i=0; i < n; i++)
+{
+    if (i == 0)
+    {
+        arr[0] = Char.ToUpper(arr[0]);
+    }
+    if (i ==n-1)
+    {
+        arr[n - 1] = Char.ToUpper(arr[n-1]);
+    }
+    if (arr[i]==' ')
+    {
+        arr[i + 1] = Char.ToUpper(arr[i + 1]);
+        arr[i - 1] = Char.ToUpper(arr[i - 1]);
+    }
+}
+string z = new string(arr);
+Console.WriteLine(z);
